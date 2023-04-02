758aacd5d3f6f156f6b1ae2efcdb7d59d85e0e1a
Subham <sc88dev0028@gmail.com>
2023-04-02 21:43:41 +0530

    Monday Done

---
+++ src/components/ProviderSelection/ProviderMap.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+
+const ProviderMap = () => {
+  return (
+    <Grid xs={3}>
+      <img
+        style={{ width: "100%", height: "400px" }}
+        src="https://i.stack.imgur.com/HILmr.png"
+      ></img>
+    </Grid>
+  );
+};
+export default ProviderMap;
